7d5d74b5fd9bd6f36edfc26270d8f81f489f10a1
Emin <emin.sarux2004@yandex.ru>
2023-10-13 19:16:58 +0300

    test asp.net project as dot net task

---
+++ WebApplication1/Models/ProductsViewModel.cs
@@ -0,0 +1,11 @@
+namespace WebApplication1.Models;
+
+public class ProductsViewModel
+{
+    public List<Product> Products { get; set; }
+}
+
+public class Product
+{
+    public string ProductName { get; set; }
+}
---
+++ WebApplication1/Views/Home/Potato.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Views.Home;
+
+public class Potato : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
