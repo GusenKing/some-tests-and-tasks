9ad7a7c21afc9b963ba449a9f31bae610692f755
Emin <emin.sarux2004@yandex.ru>
2023-10-13 12:37:11 +0300

    dot net task about two track railway approach of error handling

---
+++ some-tests-and-tasks/Program.fs
@@ -0,0 +1,77 @@
+﻿type Message =
+    | BlankName
+    | NameTooLong
+
+type Result<'TEntity> =
+    | Success of 'TEntity 
+    | Failure of Message
+
+let bind switchFunction =
+    fun twoTrackInput ->
+        match twoTrackInput with
+        | Success s -> switchFunction s
+        | Failure f -> Failure f
+
+let (>>=) twoTrackInput switchFunction =
+    bind switchFunction twoTrackInput
+
+let map singleTrackFunction twoTrackInput =
+    match twoTrackInput with
+    | Success s -> Success (singleTrackFunction s)
+    | Failure f -> Failure f
+    
+let tee deadEndFunction oneTrackInput =
+    deadEndFunction oneTrackInput
+    oneTrackInput
+
+let receiveRequest =
+    "some request"
+
+let nameNotBlank (input : string) =
+    if input.Length = 0 then Failure BlankName
+    else Success input
+
+let name50 (input : string) =
+    if input.Length > 50 then Failure NameTooLong
+    else Success input
+
+let validateRequest twoTrackInput : Result<string> =
+    twoTrackInput
+    >>= nameNotBlank
+    >>= name50
+
+let canonicalizeEmailLogic (input : string) =
+    input.ToLower().Trim()
+
+let canonicalizeEmail input =
+    map canonicalizeEmailLogic input
+
+let updateDbLogic request =
+    //some db updating logic 
+    ()
+
+let updateDbFromRequest request =
+    tee updateDbLogic request
+
+let sendEmailLogic request =
+    //email sending logic
+    ()
+
+let sendEmail request =
+    tee sendEmailLogic request
+
+let returnMessage result =
+    match result with
+    | Success s -> "success"
+    | Failure err ->
+        match err with
+        | BlankName -> "Name must not be blank"
+        | NameTooLong -> "Name mist be shorter"
+
+let updateCustomerWithErrorHandling =
+    receiveRequest
+    >> validateRequest
+    >> canonicalizeEmail
+    >> updateDbFromRequest
+    >> sendEmail
+    >> returnMessage
